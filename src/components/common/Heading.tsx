02e2ba798b5ad1865784ae851ecc4075d071b26d
lostgirljourney <falgunisarkar526@gmail.com>
2023-06-30 23:29:43 +0530

    few more fixes done 🌻

--- src/components/common/Heading.tsx
+++ src/components/common/Heading.tsx
@@ -1,6 +1,6 @@
 export const Heading = () => {
 	return (
-		<p className="font-montserrat font-bold text-4xl text-[#333] flex justify-center mb-14">
+		<p className="font-raleway font-bold text-4xl text-[#333] flex justify-center mb-14">
 			#todo
 		</p>
 	);
